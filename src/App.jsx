fe9cfbb3b9349c0c0d53b7bec962e70309902e16
mahmuodafifi1 <mahmouafifi881@gmail.com>
2025-09-27 04:01:02 -0700

    Fix contact component import - rename contact.jsx to Contact.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Header } from './components/Header'
 import { About } from './components/About'
 import { Projects } from './components/Projects'
 import { Testimonials } from './components/Testimonials'
-import { Contact } from './components/contact'
+import { Contact } from './components/Contact'
 import { Footer } from './components/Footer'
 const App = () => {
   return (
